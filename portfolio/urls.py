5e593b5093e63699b3176bbd77f425e39838449c
Haynes Cromartie <hrc245@mail.com>
2020-04-29 22:00:40 -0400

    Fixed portfolio/urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('word_setup/',include('wordcount.urls')),
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
